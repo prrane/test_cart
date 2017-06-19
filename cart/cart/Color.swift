c7b2037630bec0cf74b19c5b401b6316038d230d
Prashant Rane <prrane@microsoft.com>
2017-06-19 18:13:45 +0530

    initial  commit

---
+++ cart/cart/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright 2013-2017 Microsoft Inc.
+//
+
+import Foundation
+
+class Color {
+  public init() {
+    print("Class has been initialised")
+  }
+
+  public func cartColor() -> UIColor {
+    return .red
+  }
+}
---
+++ cart/cart/cart.h
@@ -0,0 +1,19 @@
+//
+//  cart.h
+//  cart
+//
+//  Created by Prashant Rane on 6/19/17.
+//  Copyright © 2017 prrane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cart.
+FOUNDATION_EXPORT double cartVersionNumber;
+
+//! Project version string for cart.
+FOUNDATION_EXPORT const unsigned char cartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cart/PublicHeader.h>
+
+
